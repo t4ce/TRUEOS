9e957a0a05c6ecef16d7e44208a321805e9a8fda
t4ce <jonasb@post.com>
2025-12-29 22:27:25 +0100

    structure

---
+++ src/path.rs
@@ -0,0 +1,278 @@
+#![allow(dead_code)]
+
+use alloc::{borrow::ToOwned, string::String};
+use core::borrow::Borrow;
+use core::ops::Deref;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Component<'a> {
+    RootDir,
+    CurDir,
+    ParentDir,
+    Normal(&'a str),
+}
+
+#[derive(Debug, Clone)]
+pub struct Components<'a> {
+    path: &'a str,
+    pos: usize,
+    yielded_root: bool,
+}
+
+#[repr(transparent)]
+pub struct Path {
+    inner: str,
+}
+
+pub struct PathBuf {
+    inner: String,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct StripPrefixError;
+
+impl Path {
+    pub fn new<S: AsRef<str> + ?Sized>(s: &S) -> &Path {
+        unsafe { &*(s.as_ref() as *const str as *const Path) }
+    }
+
+    pub fn as_str(&self) -> &str {
+        &self.inner
+    }
+
+    pub fn is_absolute(&self) -> bool {
+        self.as_str().starts_with('/')
+    }
+
+    pub fn is_relative(&self) -> bool {
+        !self.is_absolute()
+    }
+
+    pub fn parent(&self) -> Option<&Path> {
+        let s = self.trim_trailing_slash();
+        if s.is_empty() {
+            return None;
+        }
+        let idx = s.rfind('/')?;
+        if idx == 0 {
+            return Some(Path::new("/"));
+        }
+        Some(Path::new(&s[..idx]))
+    }
+
+    pub fn file_name(&self) -> Option<&str> {
+        let s = self.trim_trailing_slash();
+        let name = s.rsplit_once('/')
+            .map(|(_, tail)| tail)
+            .unwrap_or(s);
+        if name.is_empty() { None } else { Some(name) }
+    }
+
+    pub fn file_stem(&self) -> Option<&str> {
+        let name = self.file_name()?;
+        if let Some((stem, _)) = name.rsplit_once('.') {
+            if stem.is_empty() { None } else { Some(stem) }
+        } else {
+            Some(name)
+        }
+    }
+
+    pub fn extension(&self) -> Option<&str> {
+        let name = self.file_name()?;
+        let (_stem, ext) = name.rsplit_once('.')?;
+        if ext.is_empty() { None } else { Some(ext) }
+    }
+
+    pub fn with_extension(&self, ext: &str) -> PathBuf {
+        let mut base = match self.file_name() {
+            Some(name) => {
+                let s = self.as_str();
+                let cutoff = s.len() - name.len();
+                String::from(&s[..cutoff]) + name.split('.').next().unwrap_or("")
+            }
+            None => self.as_str().to_owned(),
+        };
+        if !ext.is_empty() {
+            base.push('.');
+            base.push_str(ext);
+        }
+        PathBuf { inner: base }
+    }
+
+    pub fn join<P: AsRef<Path>>(&self, path: P) -> PathBuf {
+        let p = path.as_ref();
+        if p.is_absolute() {
+            return PathBuf::from(p.as_str());
+        }
+        let mut buf = PathBuf::from(self.as_str());
+        buf.push(p);
+        buf
+    }
+
+    pub fn starts_with<P: AsRef<Path>>(&self, base: P) -> bool {
+        let b = base.as_ref().as_str();
+        let s = self.as_str();
+        if b == "/" {
+            return self.is_absolute();
+        }
+        if s == b {
+            return true;
+        }
+        if let Some(rest) = s.strip_prefix(b) {
+            return rest.starts_with('/') || rest.is_empty();
+        }
+        false
+    }
+
+    pub fn strip_prefix<P: AsRef<Path>>(&self, base: P) -> Result<&Path, StripPrefixError> {
+        let b = base.as_ref().as_str();
+        let s = self.as_str();
+        if b == "/" {
+            return if self.is_absolute() { Ok(self) } else { Err(StripPrefixError) };
+        }
+        if let Some(rest) = s.strip_prefix(b) {
+            if rest.is_empty() {
+                return Ok(Path::new(""));
+            }
+            if rest.starts_with('/') {
+                return Ok(Path::new(&rest[1..]));
+            }
+        }
+        Err(StripPrefixError)
+    }
+
+    pub fn components(&self) -> Components<'_> {
+        Components { path: self.as_str(), pos: 0, yielded_root: false }
+    }
+
+    fn trim_trailing_slash(&self) -> &str {
+        let s = self.as_str();
+        if s.len() > 1 { s.trim_end_matches('/') } else { s }
+    }
+}
+
+impl PathBuf {
+    pub fn new() -> Self {
+        Self { inner: String::new() }
+    }
+
+    pub fn from<S: AsRef<str>>(s: S) -> Self {
+        Self { inner: s.as_ref().to_owned() }
+    }
+
+    pub fn as_path(&self) -> &Path {
+        Path::new(self.as_str())
+    }
+
+    pub fn push<P: AsRef<Path>>(&mut self, path: P) {
+        let p = path.as_ref();
+        if p.is_absolute() {
+            self.inner.clear();
+            self.inner.push_str(p.as_str());
+            return;
+        }
+        if !self.inner.is_empty() && !self.inner.ends_with('/') {
+            self.inner.push('/');
+        }
+        self.inner.push_str(p.as_str());
+    }
+
+    pub fn pop(&mut self) -> bool {
+        let s = Path::new(self.as_str());
+        if let Some(parent) = s.parent() {
+            self.inner.truncate(parent.as_str().len());
+            true
+        } else {
+            if self.inner.is_empty() {
+                false
+            } else {
+                self.inner.clear();
+                true
+            }
+        }
+    }
+}
+
+impl AsRef<Path> for PathBuf {
+    fn as_ref(&self) -> &Path {
+        self.as_path()
+    }
+}
+
+impl AsRef<Path> for Path {
+    fn as_ref(&self) -> &Path {
+        self
+    }
+}
+
+impl Deref for PathBuf {
+    type Target = Path;
+    fn deref(&self) -> &Self::Target {
+        self.as_path()
+    }
+}
+
+impl Borrow<Path> for PathBuf {
+    fn borrow(&self) -> &Path {
+        self.as_path()
+    }
+}
+
+impl From<String> for PathBuf {
+    fn from(value: String) -> Self {
+        Self { inner: value }
+    }
+}
+
+impl From<&str> for PathBuf {
+    fn from(value: &str) -> Self {
+        Self { inner: value.to_owned() }
+    }
+}
+
+impl ToOwned for Path {
+    type Owned = PathBuf;
+    fn to_owned(&self) -> Self::Owned {
+        PathBuf::from(self.as_str())
+    }
+}
+
+impl<'a> Iterator for Components<'a> {
+    type Item = Component<'a>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if !self.yielded_root && self.path.starts_with('/') {
+            self.yielded_root = true;
+            // Skip duplicate leading slashes.
+            while self.pos < self.path.len() && self.path.as_bytes()[self.pos] == b'/' {
+                self.pos += 1;
+            }
+            return Some(Component::RootDir);
+        }
+
+        while self.pos < self.path.len() {
+            // Skip consecutive separators.
+            while self.pos < self.path.len() && self.path.as_bytes()[self.pos] == b'/' {
+                self.pos += 1;
+            }
+            if self.pos >= self.path.len() {
+                break;
+            }
+            let start = self.pos;
+            while self.pos < self.path.len() && self.path.as_bytes()[self.pos] != b'/' {
+                self.pos += 1;
+            }
+            let seg = &self.path[start..self.pos];
+            if seg.is_empty() {
+                continue;
+            }
+            return Some(match seg {
+                "." => Component::CurDir,
+                ".." => Component::ParentDir,
+                _ => Component::Normal(seg),
+            });
+        }
+
+        None
+    }
+}
