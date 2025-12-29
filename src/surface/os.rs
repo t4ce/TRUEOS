a48bb4e4ebfc12dd11761c639a7fc77cdc1cd206
t4ce <jonasb@post.com>
2025-12-30 00:11:03 +0100

    hammered this until only thread process and rt
    ofc fs and net + os just noimp stubs

---
+++ src/surface/os.rs
@@ -0,0 +1,13 @@
+pub mod raw {
+    pub type CChar = i8;
+    pub type CSchar = i8;
+    pub type CUchar = u8;
+    pub type CShort = i16;
+    pub type CUshort = u16;
+    pub type CInt = i32;
+    pub type CUint = u32;
+    pub type CLong = i64;
+    pub type CUlong = u64;
+    pub type CLonglong = i64;
+    pub type CUlonglong = u64;
+}
