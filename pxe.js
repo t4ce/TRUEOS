e013e4a3d64d0c8bc6d2caf6e63513171ba17539
t4ce <jonasb@post.com>
2026-01-17 18:13:19 +0100

    baseline font idea

--- pxe.js
+++ pxe.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 const fs = require("fs");
 const path = require("path");
+
 const { spawn } = require("child_process");
 const IFACE = "enx047bcb669593";
 const SERVER_CIDR = "192.168.55.1/24";
@@ -20,24 +21,11 @@ function run(cmd, args, label) {
   });
 }
 
-function mustExist(filePath, what) {
-  if (!fs.existsSync(filePath)) {
-    throw new Error(`${what} not found: ${filePath}`);
-  }
-}
-
 (async () => {
   try {
-    if (typeof process.getuid === "function" && process.getuid() !== 0) {
-      throw new Error("run this as root (sudo), required for ip + dnsmasq");
-    }
-
     const bootPath = path.join(TFTP_ROOT, BOOTFILE);
-    mustExist(bootPath, "PXE bootfile");
-
     await run("ip", ["link", "set", IFACE, "up"], "ip link set up");
     await run("ip", ["addr", "replace", SERVER_CIDR, "dev", IFACE], "ip addr replace");
-
     const args = [
       "--no-daemon",
       "--port=0",
@@ -53,20 +41,7 @@ function mustExist(filePath, what) {
       "--log-dhcp",
       `--dhcp-leasefile=${LEASES}`,
     ];
-
-    console.log("PXE ready:");
-    console.log(`- iface: ${IFACE}`);
-    console.log(`- server: ${SERVER_CIDR}`);
-    console.log(`- tftp root: ${TFTP_ROOT}`);
-    console.log(`- bootfile: ${BOOTFILE}`);
-    console.log(`Starting: dnsmasq ${args.join(" ")}`);
-
     const child = spawn("dnsmasq", args, { stdio: "inherit" });
-    child.on("exit", (code, signal) => {
-      console.log(`dnsmasq exited (${signal || code})`);
-    });
   } catch (err) {
-    console.error(err && err.message ? err.message : String(err));
-    process.exit(1);
   }
 })();
