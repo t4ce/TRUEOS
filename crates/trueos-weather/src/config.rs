3174e8276374558a9b90788f5274b04971d16ce1
t4ce <jonasb@post.com>
2026-02-15 23:33:11 +0100

    okay

---
+++ crates/trueos-weather/src/config.rs
@@ -0,0 +1,3 @@
+pub const ONECALL_URL: &str = "https://api.openweathermap.org/data/3.0/onecall";
+pub const OVERVIEW_URL: &str = "https://api.openweathermap.org/data/3.0/onecall/overview";
+pub const GEO_URL: &str = "http://api.openweathermap.org/geo/1.0/direct";
