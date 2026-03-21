c83ff2d4c5b88e9653cc95a8a1c65528dd644606
t4ce <jonasb@post.com>
2026-03-21 17:33:46 +0100

    ok

--- src/shell2/shell2_qjs_c4.rs
+++ src/shell2/shell2_qjs_c4.rs
@@ -3,53 +3,15 @@ use alloc::string::String;
 use alloc::vec::Vec;
 
 use super::ecma48;
+pub(crate) use super::shell2_qjs_c4_contract::{
+    Analysis, Diagnostic, ResultHint, SymbolRef, SymbolRole,
+};
 
 const COLOR_STRING: (u8, u8, u8) = (80, 210, 80);
 const COLOR_NUMBER: (u8, u8, u8) = (225, 190, 70);
 const COLOR_BOOLISH: (u8, u8, u8) = (95, 185, 235);
 const COLOR_FUNCTION: (u8, u8, u8) = (220, 120, 220);
 const COLOR_OBJECTISH: (u8, u8, u8) = (130, 170, 255);
-
-#[derive(Clone, Copy, PartialEq, Eq)]
-pub(crate) enum ResultHint {
-    String,
-    Number,
-    Boolean,
-    Null,
-    Undefined,
-    Function,
-    Object,
-    Unknown,
-}
-
-#[derive(Clone, Copy, PartialEq, Eq)]
-pub(crate) enum SymbolRole {
-    Decl,
-    Assign,
-    Read,
-    Call,
-}
-
-#[derive(Clone)]
-pub(crate) struct SymbolRef {
-    pub(crate) name: String,
-    pub(crate) role: SymbolRole,
-}
-
-#[derive(Clone)]
-pub(crate) struct Diagnostic {
-    pub(crate) code: &'static str,
-    pub(crate) message: String,
-    pub(crate) start: usize,
-    pub(crate) end: usize,
-}
-
-#[derive(Clone)]
-pub(crate) struct Analysis {
-    pub(crate) hint: ResultHint,
-    pub(crate) symbols: Vec<SymbolRef>,
-    pub(crate) diagnostic: Option<Diagnostic>,
-}
 
 #[derive(Clone, Copy, PartialEq, Eq)]
 enum TokenKind {
