a0ba6c0235685845a5784c386eaa84a77e38d2fb
t4ce <jonasb@post.com>
2026-03-22 21:11:19 +0100

    almost impossible

--- src/r/net/mod.rs
+++ src/r/net/mod.rs
@@ -6,7 +6,7 @@ use core::sync::atomic::{AtomicU32, Ordering};
 pub mod cli;
 pub mod srv;
 
-pub use cli::{dns, ftp, html, http, https, json, ntp, ping, pop3, smtp, ws};
+pub use cli::{dns, ftp, html, http, https, irc, json, ntp, ping, pop3, smtp, ws};
 pub use srv::{sntp, spop3, ssmtp, wss};
 
 use v::vnet as api;
