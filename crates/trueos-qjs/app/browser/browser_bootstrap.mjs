99d0ddba155cacb58f7d7b76a9e6ab6290aae91e
t4ce <jonasb@post.com>
2026-03-23 05:37:39 +0100

    lost

--- crates/trueos-qjs/app/browser/browser_bootstrap.mjs
+++
@@ -1,4 +0,0 @@
-try { console.log('[surfer bootstrap] import start /qjs/browser/browser.mjs'); } catch (_) {}
-import('/qjs/browser/browser.mjs').catch((e) => {
-  try { console.log('[surfer bootstrap] import failed', String(e && e.stack ? e.stack : e)); } catch (_) {}
-});
