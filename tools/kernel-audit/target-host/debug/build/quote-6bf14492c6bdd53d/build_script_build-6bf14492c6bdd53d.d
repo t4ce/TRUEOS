db8b0cc0e7a9566fbd3829bf435c73086ac46746
t4ce <jonasb@post.com>
2026-03-31 09:15:50 +0200

    okay

--- tools/kernel-audit/target-host/debug/build/quote-6bf14492c6bdd53d/build_script_build-6bf14492c6bdd53d.d
+++
@@ -1,5 +0,0 @@
-/home/t4ce/REPOS/TRUEOS/tools/kernel-audit/target-host/debug/build/quote-6bf14492c6bdd53d/build_script_build-6bf14492c6bdd53d.d: /home/t4ce/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/quote-1.0.45/build.rs
-
-/home/t4ce/REPOS/TRUEOS/tools/kernel-audit/target-host/debug/build/quote-6bf14492c6bdd53d/build_script_build-6bf14492c6bdd53d: /home/t4ce/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/quote-1.0.45/build.rs
-
-/home/t4ce/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/quote-1.0.45/build.rs:
