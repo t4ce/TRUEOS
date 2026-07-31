f8da02974511a0ba512bc3cdcf49913fc69c1bdf
t4ce <jonasb@post.com>
2026-07-31 20:42:42 +0200

    ok

--- target-dispatch-host/x86_64-unknown-linux-gnu/debug/deps/panic_abort-1fd598cd4ebe2516.d
+++
@@ -1,7 +0,0 @@
-/home/t4ce/REPOS/TRUEOS/target-dispatch-host/x86_64-unknown-linux-gnu/debug/deps/panic_abort-1fd598cd4ebe2516.d: /home/t4ce/.rustup/toolchains/nightly-2026-07-10-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/library/panic_abort/src/lib.rs
-
-/home/t4ce/REPOS/TRUEOS/target-dispatch-host/x86_64-unknown-linux-gnu/debug/deps/libpanic_abort-1fd598cd4ebe2516.rlib: /home/t4ce/.rustup/toolchains/nightly-2026-07-10-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/library/panic_abort/src/lib.rs
-
-/home/t4ce/REPOS/TRUEOS/target-dispatch-host/x86_64-unknown-linux-gnu/debug/deps/libpanic_abort-1fd598cd4ebe2516.rmeta: /home/t4ce/.rustup/toolchains/nightly-2026-07-10-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/library/panic_abort/src/lib.rs
-
-/home/t4ce/.rustup/toolchains/nightly-2026-07-10-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/library/panic_abort/src/lib.rs:
--- target-dispatch-host2/x86_64-unknown-linux-gnu/debug/deps/trueos_ttstt_cpu-36ef229072cca3c5.d
+++
@@ -1,6 +0,0 @@
-/home/t4ce/REPOS/TRUEOS/target-dispatch-host2/x86_64-unknown-linux-gnu/debug/deps/trueos_ttstt_cpu-36ef229072cca3c5.d: crates/trueos-ttstt-cpu/src/lib.rs crates/trueos-ttstt-cpu/src/quant.rs
-
-/home/t4ce/REPOS/TRUEOS/target-dispatch-host2/x86_64-unknown-linux-gnu/debug/deps/trueos_ttstt_cpu-36ef229072cca3c5: crates/trueos-ttstt-cpu/src/lib.rs crates/trueos-ttstt-cpu/src/quant.rs
-
-crates/trueos-ttstt-cpu/src/lib.rs:
-crates/trueos-ttstt-cpu/src/quant.rs:
